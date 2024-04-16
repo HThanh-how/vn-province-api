0a26345874d68ded120f98ebb99ca9284fb6a0c5
HThanh-how <thanhlt1235@gmail.com>
2024-04-16 15:34:25 +0700

    done

--- pages/api/[province]/index.js
+++ pages/api/[province]/index.js
@@ -4,14 +4,20 @@ import { cityData } from '../../../app/database/province.tsx';
 export default function handler(req, res) {
     const { province } = req.query;
 
-    // Tìm kiếm tỉnh có id tương ứng
-    const provinceData = cityData.find(provinces => provinces.code === Number(province));
+// Tìm kiếm tỉnh có id tương ứng
+let provinceData = cityData.find(provinces => provinces.code === Number(province));
 
-    if (!provinceData) {
-        // Nếu không tìm thấy tỉnh, trả về lỗi
-        return res.status(404).json({ error: "Provice not found" });
-    }
-    const districts = provinceData.districts.map(({ wards, ...district }) => district);
-    // Trả về các huyện của tỉnh
-    return res.status(200).json(districts);
+if (!provinceData) {
+    // Nếu không tìm thấy tỉnh, trả về lỗi
+    return res.status(404).json({ error: "Province not found" });
+}
+
+// Loại bỏ thông tin về xã khỏi dữ liệu huyện
+provinceData = {
+    ...provinceData,
+    districts: provinceData.districts.map(({ wards, ...district }) => district)
+};
+
+// Trả về thông tin về tỉnh và các huyện của tỉnh
+return res.status(200).json(provinceData);
 }
